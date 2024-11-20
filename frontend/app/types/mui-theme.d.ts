3c72485dbe77398462e740e18f328cba97fbafdb
Yellin <king22224444@163.com>
2024-11-21 01:48:57 +0800

    feat: integrate mui

---
+++ frontend/app/types/mui-theme.d.ts
@@ -0,0 +1,11 @@
+// mui-theme.d.ts
+import { Theme as MuiTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    vars: Record<string, any>;
+  }
+  interface ThemeOptions {
+    vars?: Record<string, any>;
+  }
+}
